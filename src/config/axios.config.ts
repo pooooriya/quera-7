01c8d2d88a42e3c7981d708096f709feb97312c4
pouriya.babaali1998@gmail.com <pouriya.babaali1998@gmail.com>
2023-02-03 12:35:47 +0330

    add interceptor for revalidate or remove token

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -3,3 +3,18 @@ export const AXIOS = axios.create({
   timeout: 20000,
   baseURL: "http://localhost:4000",
 });
+
+// AXIOS.interceptors.response.use(
+//   function (response) {
+//     // Do something with response data
+//     return response;
+//   },
+//   function (error) {
+//     if (error.statuscode === 401) {
+//       localStorage.removeItem("token");
+//       localStorage.removeItem("username");
+//     }
+//     // Do something with response error
+//     return Promise.reject(error);
+//   }
+// );
